fcfab58a986cfccc25980392f4c108498eb7d9ed
Jade <insondenisejade638@gmail.com>
2026-03-22 20:03:22 +0800

    final output

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d@pz@fvo745dxhv=2s1qbyzm5u%bh(rh!gr5eoav$_zdt2+_ws'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "b299-222-127-71-55.ngrok-free.app"
+]
 
 
 # Application definition
